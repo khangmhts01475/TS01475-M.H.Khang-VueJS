80606557b658087513e370971bca80017846c221
khangmhts01475 <khangmhts01475@gmail.com>
2026-01-21 21:33:31 +0700

    Lab 4

---
+++ Lab4/src/components/Bai4.vue
@@ -0,0 +1,53 @@
+<script setup>
+import img1 from '../assets/img1.png'
+import img2 from '../assets/img2.png'
+import img3 from '../assets/img3.png'
+
+const items = [
+  {
+    title: '8 loại rau củ quả giàu canxi',
+    content:
+      'Canxi là khoáng chất cần thiết đối với cơ thể người. Có nhiều cách để bổ sung canxi, trong đó bổ sung qua đường ăn uống là cách tốt nhất. Có 8 loại rau củ giàu canxi...',
+    image: img1
+  },
+  {
+    title: 'Các loại gia vị tốt cho sức khỏe',
+    content:
+      'Một số loại gia vị cung cấp nhiều polyphenol chống oxy hóa cao hơn các loại gia vị quen thuộc khác. Với một lượng nhỏ các món ăn đã thêm hương thơm và vị hấp dẫn...',
+    image: img2
+  },
+  {
+    title: '9 loại đậu bổ dưỡng nên dùng nhiều',
+    content:
+      'Đậu lăng, đậu nành, đậu phộng, đậu Hà Lan giàu chất xơ, protein cùng nhiều vitamin và khoáng chất giúp giảm lượng đường trong máu, tốt cho tim...',
+    image: img3
+  }
+]
+
+const viewDetail = (post) => {
+  alert(`Bạn đã chọn: ${post.title}`)
+}
+</script>
+
+<template>
+  <div class="container mt-5">
+    <h1 class="text-center mb-4">Kiến thức sức khỏe cộng đồng</h1>
+
+    <div class="row g-4">
+      <div v-for="(post, index) in items" :key="index" class="col-sm-4">
+        <div class="card h-100 shadow-sm">
+          <img :src="post.image" class="card-img-top" :alt="post.title" />
+
+          <div class="card-body d-flex flex-column">
+            <h5 class="card-title">{{ post.title }}</h5>
+            <p class="card-text">{{ post.content }}</p>
+
+            <button class="btn btn-info mt-auto" @click="viewDetail(post)">
+              Xem chi tiết
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
