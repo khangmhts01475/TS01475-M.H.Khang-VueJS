261be4409e8a867a2a0e3f5d2c4b661fb1e83f7a
khangmhts01475 <khangmhts01475@gmail.com>
2026-01-26 12:12:05 +0700

    tạo lab 5

--- Asm/src/App.vue
+++ Asm/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
-import Home from './views/post/Home.vue'
+import Login from './views/auth/Login.vue'
+import Register from './views/auth/Register.vue'
+import Home from './views/post/Home.vue';
 import PostDetail from './views/post/PostDetail.vue';
 import PostEditor from './views/post/PostEditor.vue';
 import Profile from './views/personal/Profile.vue';
+
 </script>
 
 <template>
-  <Profile />
+  <PostDetail />
 </template>
