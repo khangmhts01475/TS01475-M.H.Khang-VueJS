2d1fde64836a1217e0e06f4ee4556312e1c072da
khangmhts01475 <khangmhts01475@gmail.com>
2026-01-26 13:19:50 +0700

    lab 5 bai 3

---
+++ Lab5/src/components/Bai2.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+const userInput = ref('FPL')
+</script>
+
+<template>
+  <div class="p-5">
+    <input
+      v-model="userInput"
+      class="form-control"
+      placeholder="Nhập dữ liệu vào đây"
+    />
+
+    <p class="mt-3">
+      Giá trị bạn nhập: <b>{{ userInput }}</b>
+    </p>
+  </div>
+</template>
---
+++ Lab5/src/components/Bai3.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { reactive } from 'vue'
+
+const userInfo = reactive({
+  name: '',
+  age: '',
+  email: ''
+})
+</script>
+
+<template>
+  <div class="p-5 col-sm-8">
+    <h1>Thông tin cá nhân</h1>
+
+    <form>
+      <div class="mb-3">
+        <label>Họ và Tên</label>
+        <input
+          v-model="userInfo.name"
+          class="form-control"
+          placeholder="Nhập họ và tên"
+        >
+      </div>
+
+      <div class="mb-3">
+        <label>Tuổi</label>
+        <input
+          v-model="userInfo.age"
+          type="number"
+          class="form-control"
+          placeholder="Nhập tuổi"
+        >
+      </div>
+
+      <div class="mb-3">
+        <label>Email</label>
+        <input
+          v-model="userInfo.email"
+          type="email"
+          class="form-control"
+          placeholder="Nhập email"
+        >
+      </div>
+    </form>
+
+    <!-- Live output -->
+    <div class="mt-4 p-3 bg-dark text-white rounded">
+      <p><b>Họ và Tên:</b> {{ userInfo.name }}</p>
+      <p><b>Tuổi:</b> {{ userInfo.age }}</p>
+      <p><b>Email:</b> {{ userInfo.email }}</p>
+    </div>
+  </div>
+</template>
