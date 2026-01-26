4086079cb25a684776b3745f319e1525960bd77a
khangmhts01475 <khangmhts01475@gmail.com>
2026-01-26 13:02:33 +0700

    Lab 5 Bai 1

---
+++ Lab5/src/components/Bai1.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+
+const message = ref('Thông điệp ban đầu')
+
+const updateMessage = () => {
+  message.value = 'Thông điệp đã được thay đổi!'}
+</script>
+
+<template>
+<div class="p-5">
+
+<p>{{ message }}</p>
+<button class="btn btn-dark" @click="updateMessage">Thay đổi thông điệp</button>
+
+</div>
+
+</template>
