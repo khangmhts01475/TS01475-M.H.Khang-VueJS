e6ab30044b068dc230264a3379656e20f15e06ae
khangmhts01475 <khangmhts01475@gmail.com>
2026-01-30 15:24:03 +0700

    Lab 6 Bai 1

---
+++ Lab6/src/components/Bai1.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, computed } from 'vue'
+
+const dtb = ref('')
+
+const xepLoai = computed(() => {
+  if (dtb.value === '') return ''
+
+  const n = Number(dtb.value)
+  if (isNaN(n)) return ''
+
+  if (n < 5) return 'Yếu'
+  else if (n < 6.5) return 'Trung bình'
+  else if (n < 8) return 'Khá'
+  else if (n < 9) return 'Giỏi'
+  else return 'Xuất sắc'
+})
+</script>
+
+<template>
+  <div>
+    <h3>Nhập điểm của bạn:</h3>
+
+    <input type="number" v-model="dtb" />
+
+    <p>{{ xepLoai }}</p>
+  </div>
+</template>
---
+++ Lab6/src/components/Bai2.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {ref, computed} from 'vue'
+const month=ref('')
+const season=computed(()=> {
+if(month.value==='') return''
+
+const n=Number(month.value)
+if(isNaN(n)) return''
+
+if(n>=1||n<=3)'Mùa xuân'
+else if(n<=6) 'Mùa hè'
+else if(n<=9)'Mùa thu'
+else return 'Mùa đông'
+})
+</script>
+
+<template>
+<div>
+    <h3>Nhập tháng của năm</h3>
+
+    <input type="number" v-model="month"/>
+
+    <p>{{ month }}</p>
+</div>
+
+</template>
